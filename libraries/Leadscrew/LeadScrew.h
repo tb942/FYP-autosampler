e01654b5b5e6affa11a5ba5c04cbc7b044c81fe5
Tom Butler <tb942@hotmail.co.uk>
2020-04-11 15:00:34 +0100

    Removed unnecesary qualification

--- libraries/Leadscrew/LeadScrew.h
+++ libraries/Leadscrew/LeadScrew.h
@@ -16,7 +16,7 @@ class LeadScrew : public BasicStepperDriver {
 
 	static void SetVariables(bool extended_serial_messages);
     uint8_t operate(uint8_t next_pos);
-	uint8_t LeadScrew::LiquidLevel(uint8_t liquid_sensor_power_pin, uint8_t level_sensor_pin, bool liquid_detected_state, bool sensor_power_state, LiquidCrystal_I2C& lcd);
+	uint8_t LiquidLevel(uint8_t liquid_sensor_power_pin, uint8_t level_sensor_pin, bool liquid_detected_state, bool sensor_power_state, LiquidCrystal_I2C& lcd);
     void home();
     void begin();
     bool is_safe();
